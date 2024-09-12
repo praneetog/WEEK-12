879464715d3dfea68b40a85415a463dbb946121a
praneet <praneetkashyap001@gmail.com>
2024-09-12 10:52:24 +0530

    new commit 2

---
+++ turborepo/packages/ui/src/signin.tsx
@@ -0,0 +1,11 @@
+const signin = () => {
+  return (
+    <div>
+        <input type="text" placeholder="username" name="" id="" />
+        <input type="text" placeholder="password" name="" id="" />
+        <button>Submit</button>
+    </div>
+  )
+}
+
+export default signin
---
+++ turborepo/packages/ui/src/signup.tsx
@@ -0,0 +1,11 @@
+export function Signup(){
+  return (
+    <div>
+        <input type="text" placeholder="name" />
+        <input type="text" placeholder="username" />
+        <input type="text" placeholder="password" />
+        <button>Submit</button>
+    </div>
+  )
+}
+
